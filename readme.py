2b89c22ee925b44a0b092867086646372edfe790
mcachoni <96338218+mcachoni@users.noreply.github.com>
2022-08-10 19:06:03 -0300

    Add files via upload

---
+++ readme.py
@@ -0,0 +1,20 @@
+name = 'Maycon Cachoni'
+age = 28
+working = True
+studying = 'ASD'
+prog_ling = 'Python'
+prof = 'Officer'
+city = 'Sao Paulo'
+state = 'SP'
+country = 'Brazil'
+
+print( '-' * 60)
+print ( f"Hello, My name is {name} and i have {age} years old")
+print ( f"i`m learning {prog_ling} for hobby, and studying {studying}")
+
+if working == True:
+    print(f"I'm {prof} in {city}-{state},{country}")
+    print( '-' * 60)
+else:
+        print('maybe i can`t say to you, whats is my job')
+
